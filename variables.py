58a058a10bb15f61190bf268922a2d0c63a9ba0e
Haider <haider.salar@gmail.com>
2023-05-24 19:02:52 +0530

    hmmmm

--- variables.py
+++ variables.py
@@ -1,5 +1,8 @@
 character_name = "value"
 variable_btw= "between"
+int_val = 50
+dm_val = 1.5
+bool_val= True
 
 print("insert variable here called "+ character_name)
 print("it can also be "+ variable_btw +" strings")
