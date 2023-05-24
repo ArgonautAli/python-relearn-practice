9a11b428774f7afbe41cdfabbbad00253516b2de
Haider <haider.salar@gmail.com>
2023-05-24 18:55:05 +0530

    first

---
+++ hello.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("       /|"); 
+print("      / |");
+print("     /  |");
+print("    /   |");
+print("   /____|");
+print("bye world")
---
+++ variables.py
@@ -0,0 +1,5 @@
+character_name = "value"
+variable_btw= "between"
+
+print("insert variable here called "+ character_name)
+print("it can also be "+ variable_btw +" strings")
